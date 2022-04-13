3ed91bdbfd2a2bf7852bb2803d0847008bff0599
igalonso <igalonso@gmail.com>
2022-04-13 12:51:57 +0200

    finish?

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //import { defaultIp, apikey} from './variables.js';
 var default_video = "videos/dogs.mp4";
+var current = "dogs";
 var cat_video = "videos/cats.mp4";
 var default_region ="europe-north1";
 var uswest1 = "us-west1";
@@ -7,13 +8,18 @@ var europe_north1 = "europe-north1";
 var europe_south2 = "europe-south2";
 
 var url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey;
-function loadAnotherVideo() {
+function loadAnotherVideo(response) {
+
     var video = document.getElementsByTagName('video')[0];
     var sources = video.getElementsByTagName('source');
     if(sources[0].src.includes(default_video)){
-        sources[0].src = cat_video;
+    sources[0].src = cat_video;
+    current ="cats";
+    url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey+"&current="+current;
     }else{
-        sources[0].src = default_video;
+    sources[0].src = default_video;
+    current = "dogs";
+    url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey+"&current="+current;
     }
     video.load();
     video.play();
@@ -22,28 +28,34 @@ function loadAnotherVideo() {
 function selectUrl(region){
     if (region == europe_north1){
         defaultIp = "34.149.217.17";
-        url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey;
+        url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey+"&current="+current;
     }else if (region == uswest1){
         defaultIp = "34.149.217.17";
-        url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey;
+        url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey+"&current="+current;
     }else{
         defaultIp = "34.149.217.17";
-        url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey;
+        url = "https://"+defaultIp+".nip.io/v1/channel?apikey="+apikey+"&current="+current;
     }
 }
 
 function changeChannel(){
+    var t0 = performance.now();
+    var t1 = 0;
         fetch(url,{
-            mode: 'no-cors'
+            method: "GET",
+            mode: "no-cors"
         })
             .then(response => {
-                if(response){
-                    loadAnotherVideo();
-                }else{
-                    throw "Invalid response "+response.status;
-                }
+                response.text().then(function(result){                    
+                    loadAnotherVideo(result);
+                    t1 = performance.now();
+                    console.log(t0);
+                    console.log(t1);
+                    $("#timer").text("Time roundtrip: " + (Math.round((t1-t0)))/1000 + " seconds.");
+                    
+            });
             })
-            .catch(error => {
+            .catch(function(error) {
                 alert(error);
             });
 }
